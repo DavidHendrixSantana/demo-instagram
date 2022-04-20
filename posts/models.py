7faafcde93def26306d3bdb36c9ae3dee82c21d1
DavidHendrixSantana <dasalvasuper@gmail.com>
2022-04-20 13:09:43 -0500

    Custom User Model Migration

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    # User Model
+    email = models.EmailField(unique=True)
+    password= models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    bio = models.TextField()
+    birthdate = models.DateField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
